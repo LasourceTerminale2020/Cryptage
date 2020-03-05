93fed203445eeb19a5182db0a80ba89b3d6b35cf
mimialim <59696657+mimialim@users.noreply.github.com>
2020-03-05 16:24:11 +0100

    Update and rename encryptage.py to encodage.py

--- encodage.py
+++ encodage.py
@@ -106,7 +106,31 @@ def convertToMorseCode(phrase):
         phrasecrypte += MORSE[lettre] + " " 
     return phrasecrypte
 
-print('Le message à mettre en morse ?')
-message = input()
-morse = convertToMorseCode(message)
-print(texte)
+rep = input("Voulez-vous encrypter ou décoder un texte ? ")
+cryptage = re.search(r"\Bncrypt\B", rep) or re.search(r"\Bncod\B", rep)
+décodage = re.search(r"\Bécod\B", rep) or re.search(r"\Bécrypt\B", rep)
+
+if (cryptage):  # pour encoder un texte
+    codage = input("Quelle sorte de codage voulez-vous utiliser ? ")
+    Carre = re.search(r"\Barr\B" and r"lphabet\b", codage)
+    Morse = search("morse" or "Morse", codage)
+    if (Carre):
+        texte = input(
+            "Quel texte voulez-vous encrypter ? /!\ pas de caractères spéciaux s'il vous plaît : ) ")
+        texte = carre(texte)
+        print(texte)
+    elif (Morse):
+	print('Le message à mettre en morse ?')
+	message = input()
+	morse = convertToMorseCode(message)
+	print(texte)
+	
+elif (décodage):  # pour décoder un texte
+    décryptage = input("Quel est le texte à décoder ? ")
+# message encrypté avec des nombres et/ou des caractères spéciaux
+    specials = re.search("\d", décryptage)
+    if (specials):
+        if len(list(décryptage)) % 2 == 0:
+            texte = alphabet(décryptage)
+            print(texte)
+ #      elif (morse):
